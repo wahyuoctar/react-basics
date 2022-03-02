83275c9f26495ff72488d2c4dc1a71f64a6f1c9c
wahyuoctar <wahyuoctar@gmail.com>
2022-03-02 15:12:10 +0700

    ToDoItem with edit & delete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import TourSection from './components/TourSection/TourSection';
 import ClassComponent from './components/ClassComponent/ClassComponent';
 import ContentCard from './components/ContentCard/ContentCard';
 import ToDoItem from './components/ToDoItem/ToDoItem';
-import { Button } from 'reactstrap'
+import { Button, Input } from 'reactstrap'
 
 const data = [
   {
@@ -30,33 +30,33 @@ const data = [
   }
 ]
 
-const listToDO = [
-  {
-    date: "2 Mar 2022",
-    action: "Belajar Programming",
-    status: "Done"
-  },
-  {
-    date: "4 Mar 2022",
-    action: "Belajar Mengaji",
-    status: "On Going"
-  },
-  {
-    date: "7 Mar 2022",
-    action: "Belajar Mengemudi",
-    status: "Done"
-  },
-  {
-    date: "10 Mar 2022",
-    action: "Main Basket",
-    status: "Done"
-  },
-  {
-    date: "15 Mar 2022",
-    action: "Ngaduk Semen",
-    status: "On Going"
-  }
-]
+// const listToDO = [
+//   {
+//     date: "2 Mar 2022",
+//     action: "Belajar Programming",
+//     status: "Done"
+//   },
+//   {
+//     date: "4 Mar 2022",
+//     action: "Belajar Mengaji",
+//     status: "On Going"
+//   },
+//   {
+//     date: "7 Mar 2022",
+//     action: "Belajar Mengemudi",
+//     status: "Done"
+//   },
+//   {
+//     date: "10 Mar 2022",
+//     action: "Main Basket",
+//     status: "Done"
+//   },
+//   {
+//     date: "15 Mar 2022",
+//     action: "Ngaduk Semen",
+//     status: "On Going"
+//   }
+// ]
 
 function App() {
 
@@ -72,26 +72,102 @@ function App() {
     })
   }
 
-  const toDoItem = () =>{
-    return listToDO.map((val) =>{
+  const renderToDoItem = () =>{
+    return toDoItem.map((val, idx) =>{
       return (
         <ToDoItem
         date = {val.date}
         action = {val.action}
-        status = {val.status}
+        isDone = {val.isDone}
+        deleteData={()=> deleteItem(idx)}
+        editProgress={()=> editStatus(idx)}
         />
       )
     })
+  }
+
+  const [todoInputValue, setTodoInputValue] = useState("")
+
+  const [todoInputDateValue, setTodoInputDate] = useState("")
+
+  const [toDoItem, setToNewList] = useState(
+    [{
+      date: new Date(),
+      action: "Belajar Programming",
+      isDone: true
+    },
+    {
+      date: new Date(),
+      action: "Belajar Mengaji",
+      isDone: false
+    },
+    {
+      date: new Date(),
+      action: "Belajar Mengemudi",
+      isDone: true
+    },
+    {
+      date: new Date(),
+      action: "Main Basket",
+      isDone: true
+    },
+    {
+      date: new Date(),
+      action: "Ngaduk Semen",
+      isDone: false
+    }]
+  )
+
+  const inputHandler = (event) => {
+    const {value} = event.target
+    setTodoInputValue(value)
+  }
+
+  const inputDateHandler = (event) => {
+    const {value} = event.target
+    setTodoInputDate(value)
+  }
+
+  const addTodoItem = () => {
+    let newTodoArray = [...toDoItem]
+    newTodoArray.push({
+      date: todoInputDateValue,
+      action: todoInputValue,
+      isDone: "On Going"
+    })
+
+    setToNewList(newTodoArray)
+  }
+
+  const deleteItem = (index) => {
+    const newArray = [...toDoItem]
+    newArray.splice(index, 1)
+    setToNewList(newArray)
+  }
+
+  const editStatus = (index) => {
+    const newArray = [...toDoItem]
+    newArray[index].isDone = !newArray[index].isDone
+    setToNewList(newArray)
   }
 
   return (
     <>
     {/* <Navbar/> */}
     <div className='container'>
+      <div className='row my-3'>
+        <div className='offset-3 col-5'>
+          <Input onChange={inputHandler}/>
+          <Input type='date' onChange={inputDateHandler} />
+        </div>
+        <div className='col-2'>
+          <Button onClick={addTodoItem} color='success'>Add Todo</Button>
+        </div>
+      </div>
       <div className='row'>
         <div className='col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3'>
           {/* {renderContentList()} */}
-        {toDoItem()}
+        {renderToDoItem()}
         </div>
       </div>
     </div>
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -6,16 +6,9 @@ CardTitle,
 CardText,
 Button
 } from 'reactstrap'
+import moment from 'moment'
 
 const ToDoItem = (props) => {
-    const [status, setIsDone] = useState(props.status)
-
-    const switchButton = () => {
-        if(status == "Done"){
-            setIsDone("On Going")
-        } else{
-            setIsDone("Done")}
-    }
 
 return (
     <Card className='my-2'>
@@ -23,18 +16,20 @@ return (
             <div className="d-flex justify-content-between">
                 <div>
             <CardTitle tag='h5' className='fw-bold'>
-                {props.date}</CardTitle>
+                {moment(props.date).format("DD MMMM YYYY")}</CardTitle>
             <CardText>
                 {props.action}
             </CardText>
             </div>
-            {status == "Done" ? 
-            <Button onClick={switchButton} color='success'>
-            Done
-            </Button>: 
-            <Button onClick={switchButton} color='danger'>
-            On Going
-            </Button>}
+            {props.isDone ? (
+            <div>
+                <Button onClick={props.editProgress} color='success'>Done</Button>
+                <Button onClick={props.deleteData} color='danger'>Delete</Button>
+            </div>): 
+            (<div>
+                <Button onClick={props.editProgress} color='warning'>On Going</Button>
+                <Button onClick={props.deleteData} color='danger'>Delete</Button>
+            </div>)}
             
             </div>
         </CardBody>
